9617d20a5a671a794a6e204852655a27aca6a39e
Amanda111 <631551773@qq.com>
2016-07-26 21:56:14 +0800

    vue

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
 require('./scss/index.scss');
-// require("url-loader!./img/title.png");
 
 
 var cb = require("./js/chatbot");
 cb()
-
-var Vue=require('vue')
-
-Vue.config.delimiters = ['${', '}']
-
-var vm=new Vue({
-	el: "body",
-	data: {
-		a: 1
-	}
-})
--- js/chatbot.js
+++ js/chatbot.js
@@ -1,3 +1,25 @@
 module.exports = function() {
-	console.log('hhhh');
+//config
+var Vue=require('vue')
+var request = require('superagent');
+Vue.config.delimiters = ['${', '}']
+new Vue({
+		el: "body",
+		data: {
+			ques:'',
+			myques:[],
+		},
+		methods:{
+			ask: function(){		
+				var text = this.ques.trim()
+				if (text) {
+					this.myques.push({ text: text})
+					this.ques = ''
+				}
+				this.$nextTick(function(){
+                    this.$els.content.scrollTop = this.$els.content.scrollHeight;
+                });
+			}
+		}
+	})
 }
